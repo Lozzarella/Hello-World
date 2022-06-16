999b8b84bee810ce13eb2b729916ea9406155cee
Lozzarella <laurenbrooks70@gmail.com>
2022-06-16 17:57:55 +1000

    Saving and loading option settings
    master volume and mute toggles

--- Assets/Scripts/Main Menu/MenuHandler.cs
+++ Assets/Scripts/Main Menu/MenuHandler.cs
@@ -9,7 +9,10 @@ public class MenuHandler : MonoBehaviour//Class to manage main menu canvas syste
 {
     public Transform playerTrans;
 
+
+
     #region Main Menu System
+   
     public void ChangeScene(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
@@ -56,12 +59,16 @@ public class MenuHandler : MonoBehaviour//Class to manage main menu canvas syste
         {
             masterAudio.SetFloat(currentSlider, -80);
             tempSlider.interactable = false;
+            PlayerPrefs.SetInt(currentSlider + "mute", 1);
         }
         else
         {
             masterAudio.SetFloat(currentSlider, tempSlider.value);
             tempSlider.interactable = true;
+            PlayerPrefs.SetInt(currentSlider + "mute", 0);
         }
+
+        
     }
 
     public void CurrentSlider(string sliderName)
@@ -72,7 +79,39 @@ public class MenuHandler : MonoBehaviour//Class to manage main menu canvas syste
     public void ChangeVolume (float volume)
     {
         masterAudio.SetFloat(currentSlider, volume);
+        PlayerPrefs.SetFloat(currentSlider, volume);
     }
+
+
+    #endregion
+    #region Save and Load User Settings
+    //sliders
+    [SerializeField] Slider sliderMasterVol;
+    [SerializeField] Slider sliderMusicVol;
+    [SerializeField] Slider sliderSfxVol;
+    [SerializeField] Toggle muteMaster;
+ 
+    void LoadSettings()
+    {
+        float theSavedSetting;
+        theSavedSetting = PlayerPrefs.GetFloat("masterVolume", 80f);
+        masterAudio.SetFloat("masterVolume", theSavedSetting);
+        sliderMasterVol.value = theSavedSetting;
+        bool muted = PlayerPrefs.GetInt(currentSlider + "mute", 0) == 0 ? false: true;
+        muteMaster.isOn = muted;
+
+
+        theSavedSetting = PlayerPrefs.GetFloat("musicVolume", 80f);
+        masterAudio.SetFloat("musicVolume", theSavedSetting);
+        sliderMusicVol.value = theSavedSetting;
+
+        theSavedSetting = PlayerPrefs.GetFloat("sfxVolume", 80f);
+        masterAudio.SetFloat("sfxVolume", theSavedSetting);
+        sliderSfxVol.value = theSavedSetting;
+
+        
+    }
+
     #endregion
 
     #region Quality
@@ -112,6 +151,8 @@ public class MenuHandler : MonoBehaviour//Class to manage main menu canvas syste
         resDropdown.AddOptions(options);
         resDropdown.value = currentResolutionIndex;
         resDropdown.RefreshShownValue();
+
+        LoadSettings();
     }
 
     public void SetResolution(int resolutionIndex)
